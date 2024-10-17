7907ce5a50bf26b6b5d0a6408c92549abc508c58
Miguel Monzó <mikemonzo@gmail.com>
2024-10-17 17:16:53 +0200

    add Book and Publisher

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -2,4 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
-    title = models.TextField(max_length=255)
+    title = models.TextField(max_length=250)
+    year = models.IntegerField(max_length=4, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.year}"
+
+
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=250)
+    
+
+    def __str__(self):
+        return f"{self.name} - {self.address}"
+
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    # author = models.TextField(max_length=200)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} - {self.publication_date} - {self.publisher}"
+
