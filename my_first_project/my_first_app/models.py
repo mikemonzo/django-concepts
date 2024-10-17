d76eafe583e23e8ee2e0372a837c2879366d787d
Miguel Monzó <mikemonzo@gmail.com>
2024-10-17 19:21:32 +0200

    fix warnings

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -1,7 +1,28 @@
+"""
+This module defines the models for the Django application.
+Models:
+    Car: Represents a car with a title and year.
+    Publisher: Represents a publisher with a name and address.
+    Author: Represents an author with a name and birth date.
+    Profile: Represents a profile associated with an author, including bio and website.
+    Book: Represents a book with a title, publication date, publisher, and multiple authors.
+Each model includes a __str__ method to return a string representation of the instance.
+"""
 from django.db import models
+
 
 # Create your models here.
 class Car(models.Model):
+    """
+    Car model representing a vehicle with a title and year.
+
+    Attributes:
+        title (TextField): The title or name of the car, with a maximum length of 250 characters.
+        year (IntegerField): The manufacturing year of the car, which can be null.
+
+    Methods:
+        __str__(): Returns a string representation of the car in the format "title - year".
+    """
     title = models.TextField(max_length=250)
     year = models.IntegerField(null=True)
 
@@ -10,17 +31,33 @@ class Car(models.Model):
 
 
 class Publisher(models.Model):
+    """
+    Publisher model represents a publishing entity with a name and address.
+    Attributes:
+        name (TextField): The name of the publisher, with a maximum length of 200 characters.
+        address (TextField): The address of the publisher, with a maximum length of 250 characters.
+    Methods:
+        __str__(): Returns a string representation of the Publisher instance, 
+                combining the name and address.
+    """
     name = models.TextField(max_length=200)
     address = models.TextField(max_length=250)
-    
 
     def __str__(self):
         return f"{self.name} - {self.address}"
 
 
-
-
 class Author(models.Model):
+    """
+    Author model represents an author with a name and birth date.
+
+    Attributes:
+        name (TextField): The name of the author, with a maximum length of 200 characters.
+        birth_date (DateField): The birth date of the author.
+
+    Methods:
+        __str__(): Returns a string representation of the author in the format "name - birth_date".
+    """
     name = models.TextField(max_length=200)
     birth_date = models.DateField()
 
@@ -29,16 +66,41 @@ class Author(models.Model):
 
 
 class Profile(models.Model):
+    """
+    Profile model representing a user's profile.
+    Attributes:
+        author (OneToOneField): A one-to-one relationship to the Author model.
+        bio (TextField): A text field for the user's biography 
+                        with a maximum length of 500 characters.
+        website (URLField): A URL field for the user's website.
+    Methods:
+        __str__(): Returns a string representation of the profile, 
+                including the author, bio, and website.
+    """
     author = models.OneToOneField(Author, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500)
     website = models.URLField()
-    
 
     def __str__(self):
         return f"{self.author} - {self.bio} - {self.website}"
 
 
 class Book(models.Model):
+    """
+    Book model represents a book entity with its title, publication date, publisher, and authors.
+
+    Attributes:
+        title (TextField): The title of the book with a maximum length of 200 characters.
+        publication_date (DateField): The date when the book was published.
+        publisher (ForeignKey): A foreign key relationship to the Publisher model, 
+                                with a cascade delete option.
+        authors (ManyToManyField): A many-to-many relationship to the Author model, 
+                                    with a related name "authors".
+
+    Methods:
+        __str__(): Returns a string representation of the book, including its title, 
+                publication date, and publisher.
+    """
     title = models.TextField(max_length=200)
     publication_date = models.DateField()
     # author = models.TextField(max_length=200)
@@ -47,4 +109,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.publication_date} - {self.publisher}"
-    
--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,3 +1,7 @@
+"""
+This module contains view functions for the my_first_app Django application.
+"""
+
 from django.shortcuts import render
 
 car_listed = [
@@ -8,10 +12,29 @@ car_listed = [
 
 # Create your views here.
 def car_list(request):
+    """
+    View function to display a list of cars.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML page displaying the list of cars.
+    """
     context = {'car_list': car_listed}
     return render(request, 'my_first_app/car_list.html', context)
 
 
-def car_detail(request, id):
-    context = {'car': car_listed[id]}
+def car_detail(request, car_id):
+    """
+    View function to display the details of a specific car.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        car_id (int): The ID of the car to be displayed.
+
+    Returns:
+        HttpResponse: The rendered HTML page displaying the car details.
+    """
+    context = {'car': car_listed[car_id]}
     return render(request, 'my_first_app/car_detail.html', context)
