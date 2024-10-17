086bc1e71f38fc8a269855943ed80312b1e64f09
Miguel Monzó <mikemonzo@gmail.com>
2024-10-17 18:34:45 +0200

    add profile

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.TextField(max_length=250)
-    year = models.IntegerField(max_length=4, null=True)
+    year = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.title} - {self.year}"
@@ -26,6 +26,16 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.birth_date}"
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500)
+    website = models.URLField()
+    
+
+    def __str__(self):
+        return f"{self.author} - {self.bio} - {self.website}"
 
 
 class Book(models.Model):
