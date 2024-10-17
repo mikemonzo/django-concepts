991673fa66e11573d75a35cc885c6449e30cb4bc
Miguel Monzó <mikemonzo@gmail.com>
2024-10-17 19:13:48 +0200

    update urls

--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
+car_listed = [
+        {'title': 'Toyota Camry', 'year': 2021},
+        {'title': 'Honda Accord', 'year': 2020},
+        {'title': 'Ford Escape', 'year': 2019},
+]
+
 # Create your views here.
-def my_view(request):
-    car_list = [
-        {'title': 'Toyota Camry'},
-        {'title': 'Honda Accord'},
-        {'title': 'Ford Escape'},
-    ]
-    context = {'car_list': car_list}
+def car_list(request):
+    context = {'car_list': car_listed}
     return render(request, 'my_first_app/car_list.html', context)
+
+
+def car_detail(request, id):
+    context = {'car': car_listed[id]}
+    return render(request, 'my_first_app/car_detail.html', context)
